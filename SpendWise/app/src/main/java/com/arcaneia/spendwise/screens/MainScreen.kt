0e8cfaefe24162bcee8ae85218366cbdb6edd43f
TheXoan <juanbuenodev@gmail.com>
2025-09-25 22:30:08 +0200

    Empezando BottomNavBar...

--- SpendWise/app/src/main/java/com/arcaneia/spendwise/screens/MainScreen.kt
+++ SpendWise/app/src/main/java/com/arcaneia/spendwise/screens/MainScreen.kt
@@ -1,9 +1,12 @@
 package com.arcaneia.spendwise.screens
 
 import android.annotation.SuppressLint
+import android.widget.Space
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -11,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.arcaneia.spendwise.navigation.AppNavigation
 import com.arcaneia.spendwise.navigation.AppScreens
@@ -34,12 +38,14 @@ fun BodyContent(navController: NavController) {
             Text("TEST")
         }
     }
+    Spacer(modifier = Modifier.height(16.dp))
+
+
+
 }
 
-/*
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview(){
-    MainScreen()
+fun BottomBar(){
+
 }
-*/
